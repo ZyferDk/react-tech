56b642e462d8157d8ddde731bbccdc9c6a92d296
Surajidk <surajidk12@gmail.com>
2022-05-16 17:43:01 +0700

    update

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -14,11 +14,11 @@ const AppFooter = () => {
                     <a href="google.com">TECH</a>
                 </div>
                 <ul className="socials">
-                    <li><a href="https://www.facebook.com/"><i className="fab fa-facebook-f"></i></a></li>
-                    <li><a href="https://www.twitter.com/"><i className="fab fa-twitter"></i></a></li>
-                    <li><a href="https://www.linkedin.com/"><i className="fab fa-linkedin-in"></i></a></li>
-                    <li><a href="https://www.pinterest.com/"><i className="fab fa-pinterest-p"></i></a></li>
-                    <li><a href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a></li>
+                    <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i className="fab fa-linkedin-in"></i></a></li>
+                    <li><a href="#"><i className="fab fa-pinterest-p"></i></a></li>
+                    <li><a href="#"><i className="fab fa-instagram"></i></a></li>
                 </ul>
                 <BackTop duration="1000">
                     <div className="goTop"><i className="fas fa-arrow-circle-up"></i></div>
