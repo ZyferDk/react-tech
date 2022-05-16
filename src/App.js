35b32e21252673c4ffa17ccb37c5621cb834d792
Surajidk <surajidk12@gmail.com>
2022-05-16 18:23:40 +0700

    delete pack json

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import AppHeader from './components/common/header';
-import AppHome from './views/home';
+import AppHeader from "./components/common/header";
+import AppHome from "./views/home";
 
+import "./App.css";
+import "antd/dist/antd.css";
 
-import './App.css';
-import 'antd/dist/antd.css';
-
-import { Layout } from 'antd';
+import { Layout } from "antd";
 const { Header, Content } = Layout;
-
 
 const App = () => {
   return (
@@ -22,6 +20,6 @@ const App = () => {
       </Content>
     </Layout>
   );
-}
+};
 
 export default App;
--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -1,33 +1,52 @@
-import React from 'react';
-import { Layout, BackTop } from 'antd';
+import React from "react";
+import { Layout, BackTop } from "antd";
 
 const { Footer } = Layout;
 
-
-
 const AppFooter = () => {
-    return (
-        <Footer style={{ textAlign: 'center' }}>
-            <div className="footer">
-                <div className="logo">
-                    <i className="fas fa-bolt"></i>
-                    <a href="google.com">TECH</a>
-                </div>
-                <ul className="socials">
-                    <li><a href="https://www.facebook.com/"><i className="fab fa-facebook-f"></i></a></li>
-                    <li><a href="https://www.twitter.com/"><i className="fab fa-twitter"></i></a></li>
-                    <li><a href="https://www.linkedin.com/"><i className="fab fa-linkedin-in"></i></a></li>
-                    <li><a href="https://www.pinterest.com/"><i className="fab fa-pinterest-p"></i></a></li>
-                    <li><a href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a></li>
-                </ul>
-                <BackTop duration="1000">
-                    <div className="goTop"><i className="fas fa-arrow-circle-up"></i></div>
-                </BackTop>
-            </div>
-
-            Copyright © 2020 Tech
-        </Footer>
-    )
-}
+  return (
+    <Footer style={{ textAlign: "center" }}>
+      <div className="footer">
+        <div className="logo">
+          <i className="fas fa-bolt"></i>
+          <a href="google.com">TECH</a>
+        </div>
+        <ul className="socials">
+          <li>
+            <a href="https://www.facebook.com/">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com/">
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/">
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.pinterest.com/">
+              <i className="fab fa-pinterest-p"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </li>
+        </ul>
+        <BackTop duration="1000">
+          <div className="goTop">
+            <i className="fas fa-arrow-circle-up"></i>
+          </div>
+        </BackTop>
+      </div>
+      Copyright © 2020 Tech
+    </Footer>
+  );
+};
 
 export default AppFooter;
